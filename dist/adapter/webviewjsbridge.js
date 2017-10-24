6f2de63cd3c35f6c012de353d8e4af1501efaf36
duwen <duwen-pbj@sinosig.com>
2017-10-24 11:50:37 +0800

    update WebViewJavascriptBridge  adapter

--- dist/adapter/webviewjsbridge.js
+++ dist/adapter/webviewjsbridge.js
@@ -82,14 +82,32 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, exports) {
 
 function KEEP(_,cb){cb();}
-"use strict";
+'use strict';
 
-function adapter(bridge) {
-    return function (request, responseCallBack) {
+//reference from  https://github.com/marcuswestin/WebViewJavascriptBridge
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) {
+        return callback(WebViewJavascriptBridge);
+    }
+    if (window.WVJBCallbacks) {
+        return window.WVJBCallbacks.push(callback);
+    }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function () {
+        document.documentElement.removeChild(WVJBIframe);
+    }, 0);
+}
+
+function adapter(request, responseCallBack) {
+    setupWebViewJavascriptBridge(function (bridge) {
         bridge.callHandler("onAjaxRequest", request, function (response) {
             responseCallBack(response);
         });
-    };
+    });
 }
 //build环境定义全局变量
 ;
--- src/adapter/webviewjsbridge.js
+++ src/adapter/webviewjsbridge.js
@@ -1,9 +1,22 @@
-function adapter(bridge) {
-    return function (request, responseCallBack) {
-        bridge.callHandler("onAjaxRequest",request,(response)=>{
-            responseCallBack(response)
-        })
-    }
+
+//reference from  https://github.com/marcuswestin/WebViewJavascriptBridge
+function setupWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
+    if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() { document.documentElement.removeChild(WVJBIframe) }, 0)
+}
+
+function adapter(request, responseCallBack) {
+     setupWebViewJavascriptBridge(function (bridge) {
+         bridge.callHandler("onAjaxRequest",request,(response)=>{
+             responseCallBack(response)
+         })
+     })
 }
 //build环境定义全局变量
 KEEP("build", () => {
