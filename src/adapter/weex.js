f10ae80757aab335b4f247e2bcd44b6056cec1f9
chenls <chenli_1989@foxmail.com>
2020-08-10 14:50:03 +0800

    pref(weex.js): 优化请求成功与失败返回数据结构保持一致

--- src/adapter/weex.js
+++ src/adapter/weex.js
@@ -5,18 +5,11 @@ module.exports = function (request, responseCallback) {
         delete request.body;
     }
     stream.fetch(request, function (res) {
-        if (res.ok) {
-            responseCallback({
-                statusCode: res.status,
-                responseText: res.data,
-                headers: res.headers,
-                statusMessage: res.statusText
-            })
-        } else {
-            responseCallback({
-                statusCode: res.status || 0,
-                statusMessage: res.statusText
-            })
-        }
+        responseCallback({
+            statusCode: res.status || 0,
+            responseText: res.data,
+            headers: res.headers,
+            statusMessage: res.statusText
+        })
     });
 }
